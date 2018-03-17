9043fa73093fed372f05280cdf1c2982d88406be
Wisdomb33r <info@final-fantasy.ch>
2018-03-17 18:37:19 +0100

    Displaying unit stats.

--- src/app/unit-details/unit-details.component.ts
+++ src/app/unit-details/unit-details.component.ts
@@ -1,19 +1,25 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
 import {Unit} from '../../core/model/unit.model';
+import {DatabaseClientService} from '../../core/services/database-client.service';
 
 @Component({
   selector: 'app-unit-details',
   templateUrl: './unit-details.component.html',
   styleUrls: ['./unit-details.component.css']
 })
-export class UnitDetailsComponent implements OnInit {
+export class UnitDetailsComponent implements OnInit, OnChanges {
 
   @Input() unit: Unit;
+  public detailedUnit: Unit;
 
-  constructor() {
+  constructor(private databaseClient: DatabaseClientService) {
   }
 
   ngOnInit() {
   }
 
+  ngOnChanges() {console.log('loading detailed char');
+    this.databaseClient.getUnitById$(this.unit.id)
+      .subscribe((unit: Unit) => this.detailedUnit = unit);
+  }
 }
--- src/core/model/unit.model.ts
+++ src/core/model/unit.model.ts
@@ -1,7 +1,14 @@
 export class Unit {
   public id: number;
   public name: string;
+  public rank: number;
   public icon: string;
+  public hp: number;
+  public mp: number;
+  public atk: number;
+  public mag: number;
+  public def: number;
+  public spr: number;
 
   constructor() {
   }
