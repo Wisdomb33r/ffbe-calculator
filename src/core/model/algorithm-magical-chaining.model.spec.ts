dfde9c1763d6d9448787ff37564f8d0e31c0992f
Wisdomb33r <info@final-fantasy.ch>
2018-05-03 14:36:58 +0200

    Register magical chaining algorithm in factory. Magical chaining unit testing.

---
+++ src/core/model/algorithm-magical-chaining.model.spec.ts
@@ -0,0 +1,37 @@
+import {Unit} from './unit.model';
+import {CalculatorTestutils} from '../calculator-testutils.spec';
+import {AlgorithmMagicalChaining} from './algorithm-magical-chaining.model';
+import {AlgorithmResultMagicalChaining} from './algorithm-result-magical-chaining.model';
+
+const UNIT_STATS_TEST_DATA = '{"hp":3000,"mp":300,"atk":200,"mag":200,"def":100,"spr":100}';
+const EQUIPMENT_TEST_DATA = '{"id":1,"category":1,"mag":100}';
+const EQUIPMENTS_TEST_DATA = '{"right_hand":' + EQUIPMENT_TEST_DATA + ',"head":' + EQUIPMENT_TEST_DATA + ',"body":' + EQUIPMENT_TEST_DATA + ',"accessory1":' + EQUIPMENT_TEST_DATA + ',"accessory2":' + EQUIPMENT_TEST_DATA + ',"materia1":' + EQUIPMENT_TEST_DATA + ',"materia2":' + EQUIPMENT_TEST_DATA + ',"materia3":' + EQUIPMENT_TEST_DATA + ',"materia4":' + EQUIPMENT_TEST_DATA + '}';
+const SKILLS_TEST_DATA = '[{"power":500,"hits":5,"frames":"10 20 30 40 50","damages":"10 20 30 30 10"},{"power":1000,"hits":4,"frames":"10 20 30 40","damages":"10 20 30 40"}]';
+const BUILD_TEST_DATA = '{"algorithmId":2,"equipments":' + EQUIPMENTS_TEST_DATA + ',"skills":' + SKILLS_TEST_DATA + '}';
+const UNIT_TEST_DATA = '{"id":9999,"stats":' + UNIT_STATS_TEST_DATA + ',"builds":[' + BUILD_TEST_DATA + ']}';
+
+describe('AlgorithmMagicalChaining', () => {
+
+  it('#calculate should set the result object values', () => {
+    // GIVEN
+    const algorithm = new AlgorithmMagicalChaining();
+    const unit = new Unit(JSON.parse(UNIT_TEST_DATA));
+    unit.selectDefaultBuild();
+    unit.stats.mag.total = 1000;
+    const result = algorithm.calculate(unit);
+    // THEN
+    expect(result).toBeTruthy();
+    expect(result instanceof AlgorithmResultMagicalChaining).toBeTruthy();
+    expect(result.result).toBeCloseTo(28.105);
+    expect(result['mag']).toEqual(1000);
+    expect(result['buffedMag']).toEqual(1200);
+    expect(result['combosIncrement']).toEqual(0.1);
+    expect(result['averageTurnPower']).toEqual(1055);
+    expect(result['preSprDamages']).toEqual(30384000);
+    expect(result['perTurnHitsPower'][0].length).toEqual(5);
+    expect(result['perTurnHitsPower'][1].length).toEqual(4);
+    CalculatorTestutils.expectArrayOfArrayOfNumberToBeCloseTo(result['perTurnHitsPower'], [
+      [50, 120, 210, 240, 90], [100, 240, 420, 640]
+    ]);
+  });
+});
