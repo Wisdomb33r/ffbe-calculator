2d9c1bb7564cd0f8615228ade17cd8fe856e30a2
Wisdomb33r <info@final-fantasy.ch>
2018-05-15 11:45:35 +0200

    Implementing finisher algorithm. Combos chain multiplier now reset to 0 based on the chain-breaking field of a skill.

--- src/core/model/algorithm-finish.model.ts
+++ src/core/model/algorithm-finish.model.ts
@@ -1,20 +1,22 @@
 import {Algorithm} from './algorithm.model';
-import {Result} from './result.model';
 import {Unit} from './unit.model';
-import {isNullOrUndefined} from 'util';
-import {Skill} from './skill.model';
-import {ResultChaining} from './result-chaining.model';
 import {ResultOffensive} from './result-offensive.model';
+import {Skill} from './skill.model';
+import {Result} from './result.model';
+import {ResultChaining} from './result-chaining.model';
+import {isNullOrUndefined} from 'util';
 
-export class AlgorithmChaining implements Algorithm {
+export class AlgorithmFinish implements Algorithm {
 
   public isKillerActive = true;
   public isSparkChain = false;
   public isSupportBuffing = true;
+  public isSupportBreakingResistances = true;
   public supportBuff = 100;
   public opponentDef = 1000000;
   public opponentSpr = 1000000;
-  public opponentResistances: Array<number> = [-50, -50, -50, -50, -50, -50, -50, -50];
+  public supportResistsBreak: Array<number> = [-50, -50, -50, -50, -50, -50, -50, -50];
+  public opponentResistances: Array<number> = [0, 0, 0, 0, 0, 0, 0, 0];
 
   public calculate(unit: Unit): Result {
     const result: ResultOffensive = new ResultOffensive();
@@ -28,54 +30,28 @@ export class AlgorithmChaining implements Algorithm {
   private calculateTurn(skill: Skill, unit: Unit): ResultChaining {
     const result: ResultChaining = new ResultChaining();
     skill.damageType.calculateBuffs(unit, this.isSupportBuffing, this.supportBuff, result);
-    this.calculateCombosIncrement(skill, unit, result);
     this.calculateHitsPower(skill, unit, result);
     skill.damageType.calculateDamages(unit, result);
     skill.damageType.calculateKillerDamages(unit, this.isKillerActive, skill.skillType.getActiveKillers(unit), result);
+    // this.calculateEffectiveResistances(skill, result);
     skill.damageType.calculateElementalDamages(unit, skill.skillType.getElements(skill, unit), this.opponentResistances, result);
     skill.damageType.calculateFinalResult(unit, this.opponentDef, this.opponentSpr, result);
     return result;
   }
 
-  private calculateCombosIncrement(skill: Skill, unit: Unit, result: ResultChaining) {
-    let increment = 0.1;
-    const elements = skill.skillType.getElements(skill, unit);
-    if (Array.isArray(elements) && elements.length > 0) {
-      increment += 0.2 * elements.length;
-    }
-    if (this.isSparkChain) {
-      increment += 0.15;
-    }
-    result.combosIncrement = increment;
-  }
-
   private calculateHitsPower(skill: Skill, unit: Unit, result: ResultChaining) {
-    if (isNullOrUndefined(skill.hits) || skill.hits <= 1) {
+    if (isNullOrUndefined(skill.hits) || skill.hits < 1) {
       result.hitsPower = [0];
       result.power = 0;
     } else {
-      const frames: Array<number> = skill.frames.split(' ').map((s: string) => +s);
+      result.combosIncrement = 4;
       const damages: Array<number> = skill.damages.split(' ').map((s: string) => +s);
       const hitsPower: Array<number> = [];
-      let chainCombos = 0;
-      for (let i = 0; i < skill.hits; i++) {
-        if (i > 0 && frames[i] - frames[i - 1] > 25) {
-          chainCombos = 0;
-        }
-        hitsPower.push(skill.power * damages[i] / 100 * Math.min(4, 1 + chainCombos * result.combosIncrement * 2));
-        chainCombos++;
-      }
       if (skill.isBreakingChain) {
-        chainCombos = 0;
+        result.combosIncrement = 2.5;
       }
-      if (skill.skillType.isExecutingTwice(skill, unit)) {
-        for (let j = 0; j < skill.hits; j++) {
-          if (j > 0 && frames[j] - frames[j - 1] > 25) {
-            chainCombos = 0;
-          }
-          hitsPower.push(skill.power * damages[j] / 100 * Math.min(4, 1 + chainCombos * result.combosIncrement * 2));
-          chainCombos++;
-        }
+      for (let i = 0; i < skill.hits; i++) {
+        hitsPower.push(skill.power * damages[i] / 100 * result.combosIncrement);
       }
       result.hitsPower = hitsPower;
       result.power = hitsPower.reduce((val1, val2) => val1 + val2, 0);
