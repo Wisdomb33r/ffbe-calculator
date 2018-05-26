cbc0c2aa172b9425416ee6f025e119dda7ec0ef4
Wisdomb33r <info@final-fantasy.ch>
2018-05-26 15:19:53 +0200

    Bugfix: Strikethrough DH and TDH bonuses in equipment display if DH / TDH are not active respectively.

--- src/app/equipment-details/equipment-details.component.ts
+++ src/app/equipment-details/equipment-details.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {Equipment} from '../../core/model/equipment.model';
 import {ConditionalPassive} from '../../core/model/conditional-passive.model';
+import {UnitsService} from '../../core/services/units.service';
 
 @Component({
   selector: 'app-equipment-details',
@@ -11,8 +12,19 @@ export class EquipmentDetailsComponent {
 
   @Input() equipment: Equipment;
 
+  constructor(public unitsService: UnitsService) {
+  }
+
   public getConditionalPassivesToDisplay(): Array<ConditionalPassive> {
     return this.equipment.conditional_passives
       .filter(condPassive => condPassive.active || condPassive.category > 0);
+  }
+
+  public isDhActive(): boolean {
+    return this.unitsService.getEquipments().isDoubleHandActive();
+  }
+
+  public isTdhActive(): boolean {
+    return this.unitsService.getEquipments().isTrueDoubleHandActive();
   }
 }
