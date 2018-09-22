50f7f8ac3bd7b317e7a5a61e870d20a9d9b0455c
Wisdomb33r <info@final-fantasy.ch>
2018-09-22 16:41:51 +0200

    Reberta DW with trust ability.

--- src/core/model/unit.model.ts
+++ src/core/model/unit.model.ts
@@ -59,6 +59,7 @@ export class Unit {
       || this.id === 844 // CG Jake
       || this.id === 942 // CG Raegen
       || this.id === 1004 // Estark
+      || (this.id === 8053 && this.selectedBuild.equipments.right_hand && this.selectedBuild.equipments.right_hand.id === 1305) // Reberta
       || this.id === 8063 // A2
       || this.id === 8159 // Rico
       || this.id === 8168 // Adam Jensen
