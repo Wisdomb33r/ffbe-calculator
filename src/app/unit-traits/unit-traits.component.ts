700415e1cdf327d0b91600b3ee20f79220832003
Wisdomb33r <info@final-fantasy.ch>
2018-11-10 14:52:15 +0100

    Finalize unit conditional traits display.

--- src/app/unit-traits/unit-traits.component.ts
+++ src/app/unit-traits/unit-traits.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {ConditionalPassive} from '../../core/model/conditional-passive.model';
 import {MONSTER_TYPES} from '../../core/calculator-constants';
+import {UnitStats} from '../../core/model/unit-stats.model';
 
 @Component({
   selector: 'app-unit-traits',
