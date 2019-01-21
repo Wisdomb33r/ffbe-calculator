b1d677dc9429e1c8787d960524e9cef8b76ab91f
Wisdomb33r <info@final-fantasy.ch>
2019-01-21 10:58:31 +0100

    Adapting unit-details-stat-sum component tests to recent code changes.

--- src/app/unit-details-stat-sum/unit-details-stat-sum.component.spec.ts
+++ src/app/unit-details-stat-sum/unit-details-stat-sum.component.spec.ts
@@ -113,15 +113,15 @@ describe('UnitDetailsStatSumComponent', () => {
 
     // equipment passive line
     const equipmentPassiveLine = table.querySelector('tr:nth-child(6)');
-    expect(equipmentPassiveLine.querySelector('td:nth-child(1)').textContent).toEqual('unit.details.stats.popup.passive');
+    expect(equipmentPassiveLine.querySelector('td:nth-child(1)').textContent).toEqual('unit.details.stats.popup.base');
     expect(equipmentPassiveLine.querySelector('td:nth-child(2)').textContent).toEqual('500');
-    expect(equipmentPassiveLine.querySelector('td:nth-child(3)').textContent.trim()).toEqual('(50%)');
+    expect(equipmentPassiveLine.querySelector('td:nth-child(3)').textContent).toEqual('');
 
     // equipment base line
     const equipmentBaseLine = table.querySelector('tr:nth-child(7)');
-    expect(equipmentBaseLine.querySelector('td:nth-child(1)').textContent).toEqual('unit.details.stats.popup.base');
+    expect(equipmentBaseLine.querySelector('td:nth-child(1)').textContent).toEqual('unit.details.stats.popup.passive');
     expect(equipmentBaseLine.querySelector('td:nth-child(2)').textContent).toEqual('500');
-    expect(equipmentBaseLine.querySelector('td:nth-child(3)').textContent).toEqual('');
+    expect(equipmentBaseLine.querySelector('td:nth-child(3)').textContent).toEqual('(50%)');
 
     // equipment dw line
     const equipmentDwLine = table.querySelector('tr:nth-child(8)');
@@ -135,21 +135,27 @@ describe('UnitDetailsStatSumComponent', () => {
 
     // esper base line
     const esperBaseLine = table.querySelector('tr:nth-child(10)');
-    expect(esperBaseLine.querySelector('td:nth-child(1)').textContent).toEqual(''); // TODO would it not be better to display 'unit.details.stats.popup.base'
-    expect(esperBaseLine.querySelector('td:nth-child(2)').textContent).toEqual('105');
+    expect(esperBaseLine.querySelector('td:nth-child(1)').textContent).toEqual('Ifrit');
+    expect(esperBaseLine.querySelector('td:nth-child(2)').textContent).toEqual('75');
     expect(esperBaseLine.querySelector('td:nth-child(3)').textContent).toEqual('');
 
     // esper passive line
     const esperPassiveLine = table.querySelector('tr:nth-child(11)');
     expect(esperPassiveLine.querySelector('td:nth-child(1)').textContent).toEqual('unit.details.stats.popup.passive');
     expect(esperPassiveLine.querySelector('td:nth-child(2)').textContent).toEqual('0');
-    expect(esperPassiveLine.querySelector('td:nth-child(3)').textContent.trim()).toEqual('(0%)');
+    expect(esperPassiveLine.querySelector('td:nth-child(3)').textContent).toEqual('(0%)');
+
+    // esper percent line
+    const esperPercentLine = table.querySelector('tr:nth-child(12)');
+    expect(esperPercentLine.querySelector('td:nth-child(1)').textContent).toEqual('unit.details.stats.popup.esperPercent');
+    expect(esperPercentLine.querySelector('td:nth-child(2)').textContent).toEqual('30');
+    expect(esperPercentLine.querySelector('td:nth-child(3)').textContent).toEqual('(40%)');
 
     // total line
-    const totalLine = table.querySelector('tr:nth-child(12)');
+    const totalLine = table.querySelector('tr:nth-child(13)');
     expect(totalLine.querySelector('th').textContent).toEqual('unit.details.stats.popup.total');
     expect(totalLine.querySelector('td:nth-child(2)').textContent).toEqual('3,480');
-    expect(totalLine.querySelector('td:nth-child(3)').textContent.trim()).toEqual('');
+    expect(totalLine.querySelector('td:nth-child(3)').textContent).toEqual('');
   });
 
   it('should display unit stats for DH and warning for exceeding limit', () => {
@@ -159,12 +165,14 @@ describe('UnitDetailsStatSumComponent', () => {
       "hp_passive":100,"mp_passive":10,"atk_passive":10,"mag_passive":10,"def_passive":10,"spr_passive":10,
       "hp_dh":100,"mp_dh":10,"atk_dh":10,"mag_dh":10,"def_dh":10,"spr_dh":10,
       "hp_tdh":100,"mp_tdh":10,"atk_tdh":10,"mag_tdh":10,"def_tdh":10,"spr_tdh":10,
-      "hp_dw":50,"mp_dw":10,"atk_dw":10,"mag_dw":10,"def_dw":10,"spr_dw":10
+      "hp_dw":50,"mp_dw":10,"atk_dw":10,"mag_dw":10,"def_dw":10,"spr_dw":10,
+      "esper_percent": 20
     }`;
     const unitStats: UnitStats = new UnitStats(JSON.parse(UNIT_STATS_TEST_DATA));
     unitStats.defineEquipmentsStats(500, 100, 100, 100, 100, 100, 10);
     unitStats.defineEquipmentPassives(50, 10, 10, 10, 10, 10, 10, []);
     unitStats.defineEquipmentDhBonuses(250, 10, 10, 10, 10, 10, []);
+    unitStats.defineEquipmentEsperPercent(50, []);
     unitStats.defineEsperStats(GOLEM_TANKING);
     unitStats.computeTotals(true, true, false);
     const dialogConfig = {
@@ -201,21 +209,21 @@ describe('UnitDetailsStatSumComponent', () => {
     expect(unitPassiveLine.querySelector('td:nth-child(2)').textContent).toEqual('1,000');
     expect(unitPassiveLine.querySelector('td:nth-child(3)').textContent).toEqual('(100%)');
 
-    // unit dw line
-    const unitDwLine = table.querySelector('tr:nth-child(4)');
-    expect(unitDwLine.querySelector('td:nth-child(1)').textContent).toEqual('unit.details.stats.popup.dh');
-    expect(unitDwLine.querySelector('td:nth-child(2)').textContent).toEqual('1,000');
-    expect(unitDwLine.querySelector('td:nth-child(3)').textContent).toEqual('(200%)');
+    // unit dh line
+    const unitDhLine = table.querySelector('tr:nth-child(4)');
+    expect(unitDhLine.querySelector('td:nth-child(1)').textContent).toEqual('unit.details.stats.popup.dh');
+    expect(unitDhLine.querySelector('td:nth-child(2)').textContent).toEqual('1,000');
+    expect(unitDhLine.querySelector('td:nth-child(3)').textContent).toEqual('(200%)');
+
+    // unit esper percent line
+    const unitEsperPercentLine = table.querySelector('tr:nth-child(5)');
+    expect(unitEsperPercentLine.querySelector('td:nth-child(1)').textContent).toEqual('unit.details.stats.popup.esperPercent');
+    expect(unitEsperPercentLine.querySelector('td:nth-child(2)').textContent).toEqual('19');
+    expect(unitEsperPercentLine.querySelector('td:nth-child(3)').textContent).toEqual('(20%)');
 
     // equipment header line
-    const equipmentHeaderLine = table.querySelector('tr:nth-child(5)');
+    const equipmentHeaderLine = table.querySelector('tr:nth-child(6)');
     expect(equipmentHeaderLine.querySelector('th').textContent).toEqual('unit.details.stats.popup.equipment');
-
-    // equipment passive line
-    const equipmentPassiveLine = table.querySelector('tr:nth-child(6)');
-    expect(equipmentPassiveLine.querySelector('td:nth-child(1)').textContent).toEqual('unit.details.stats.popup.passive');
-    expect(equipmentPassiveLine.querySelector('td:nth-child(2)').textContent).toEqual('500');
-    expect(equipmentPassiveLine.querySelector('td:nth-child(3)').textContent.trim()).toEqual('(50%)');
 
     // equipment base line
     const equipmentBaseLine = table.querySelector('tr:nth-child(7)');
@@ -223,32 +231,50 @@ describe('UnitDetailsStatSumComponent', () => {
     expect(equipmentBaseLine.querySelector('td:nth-child(2)').textContent).toEqual('500');
     expect(equipmentBaseLine.querySelector('td:nth-child(3)').textContent).toEqual('');
 
-    // equipment dw line
-    const equipmentDwLine = table.querySelector('tr:nth-child(8)');
-    expect(equipmentDwLine.querySelector('td:nth-child(1)').textContent).toEqual('unit.details.stats.popup.dh');
-    expect(equipmentDwLine.querySelector('td:nth-child(2)').textContent).toEqual('500');
-    expect(equipmentDwLine.querySelector('td.limit-warn').textContent).toEqual('(250%)');
+    // equipment passive line
+    const equipmentPassiveLine = table.querySelector('tr:nth-child(8)');
+    expect(equipmentPassiveLine.querySelector('td:nth-child(1)').textContent).toEqual('unit.details.stats.popup.passive');
+    expect(equipmentPassiveLine.querySelector('td:nth-child(2)').textContent).toEqual('500');
+    expect(equipmentPassiveLine.querySelector('td:nth-child(3)').textContent).toEqual('(50%)');
+
+    // equipment dh line
+    const equipmentDhLine = table.querySelector('tr:nth-child(9)');
+    expect(equipmentDhLine.querySelector('td:nth-child(1)').textContent).toEqual('unit.details.stats.popup.dh');
+    expect(equipmentDhLine.querySelector('td:nth-child(2)').textContent).toEqual('500');
+    expect(equipmentDhLine.querySelector('td.limit-warn').textContent).toEqual('(250%)');
+
+    // equipment esper percent line
+    const equipmentEsperPercentLine = table.querySelector('tr:nth-child(10)');
+    expect(equipmentEsperPercentLine.querySelector('td:nth-child(1)').textContent).toEqual('unit.details.stats.popup.esperPercent');
+    expect(equipmentEsperPercentLine.querySelector('td:nth-child(2)').textContent).toEqual('47');
+    expect(equipmentEsperPercentLine.querySelector('td:nth-child(3)').textContent).toEqual('(50%)');
 
     // esper header line
-    const esperHeaderLine = table.querySelector('tr:nth-child(9)');
+    const esperHeaderLine = table.querySelector('tr:nth-child(11)');
     expect(esperHeaderLine.querySelector('th').textContent).toEqual('unit.details.stats.popup.esper');
 
     // esper base line
-    const esperBaseLine = table.querySelector('tr:nth-child(10)');
-    expect(esperBaseLine.querySelector('td:nth-child(1)').textContent).toEqual(''); // TODO would it not be better to display 'unit.details.stats.popup.base'
-    expect(esperBaseLine.querySelector('td:nth-child(2)').textContent).toEqual('130');
+    const esperBaseLine = table.querySelector('tr:nth-child(12)');
+    expect(esperBaseLine.querySelector('td:nth-child(1)').textContent).toEqual('Golem');
+    expect(esperBaseLine.querySelector('td:nth-child(2)').textContent).toEqual('93');
     expect(esperBaseLine.querySelector('td:nth-child(3)').textContent).toEqual('');
 
     // esper passive line
-    const esperPassiveLine = table.querySelector('tr:nth-child(11)');
+    const esperPassiveLine = table.querySelector('tr:nth-child(13)');
     expect(esperPassiveLine.querySelector('td:nth-child(1)').textContent).toEqual('unit.details.stats.popup.passive');
     expect(esperPassiveLine.querySelector('td:nth-child(2)').textContent).toEqual('100');
-    expect(esperPassiveLine.querySelector('td:nth-child(3)').textContent.trim()).toEqual('(10%)');
+    expect(esperPassiveLine.querySelector('td:nth-child(3)').textContent).toEqual('(10%)');
+
+    // esper esper percent line
+    const esperEsperPercentLine = table.querySelector('tr:nth-child(14)');
+    expect(esperEsperPercentLine.querySelector('td:nth-child(1)').textContent).toEqual('unit.details.stats.popup.esperPercent');
+    expect(esperEsperPercentLine.querySelector('td:nth-child(2)').textContent).toEqual('37');
+    expect(esperEsperPercentLine.querySelector('td:nth-child(3)').textContent).toEqual('(40%)');
 
     // total line
-    const totalLine = table.querySelector('tr:nth-child(12)');
+    const totalLine = table.querySelector('tr:nth-child(15)');
     expect(totalLine.querySelector('th').textContent).toEqual('unit.details.stats.popup.total');
-    expect(totalLine.querySelector('td:nth-child(2)').textContent).toEqual('4,730');
-    expect(totalLine.querySelector('td:nth-child(3)').textContent.trim()).toEqual('');
+    expect(totalLine.querySelector('td:nth-child(2)').textContent).toEqual('4,795');
+    expect(totalLine.querySelector('td:nth-child(3)').textContent).toEqual('');
   });
 });
