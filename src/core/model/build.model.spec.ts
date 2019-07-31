6037e5c5ea36c88ebf1765882689b13f3086bd76
Wisdomb33r <info@final-fantasy.ch>
2019-07-31 15:31:56 +0200

    Impriving unit tests.

--- src/core/model/build.model.spec.ts
+++ src/core/model/build.model.spec.ts
@@ -6,25 +6,28 @@ describe('Build', () => {
 
   it('should be defensive algorithm for id 8', () => {
     // GIVEN
-    const plainObj = JSON.parse('{"id": 1, "algorithmId": 8, "equipments": {}}');
+    const build: Build = new Build(JSON.parse('{"id": 1, "algorithmId": 8, "equipments": {}}'));
     // WHEN
-    const build: Build = new Build(plainObj);
+    const isDefensive: boolean = build.isDefensiveBuild();
     // THEN
     expect(build.algorithm).toBeTruthy();
+    expect(isDefensive).toBeTruthy();
     expect(build.algorithm instanceof AlgorithmDefensive).toBeTruthy();
   });
 
   it('should be offensive algorithm for id\'s other than 8', () => {
     // GIVEN
-    const plainObj7 = JSON.parse('{"id": 1, "algorithmId": 7, "equipments": {}}');
-    const plainObj9 = JSON.parse('{"id": 1, "algorithmId": 9, "equipments": {}}');
+    const build7: Build = new Build(JSON.parse('{"id": 1, "algorithmId": 7, "equipments": {}}'));
+    const build9: Build = new Build(JSON.parse('{"id": 1, "algorithmId": 9, "equipments": {}}'));
     // WHEN
-    const build7: Build = new Build(plainObj7);
-    const build9: Build = new Build(plainObj9);
+    const isOffensive7: boolean = build7.isOffensiveBuild();
+    const isOffensive9: boolean = build9.isOffensiveBuild();
     // THEN
     expect(build7.algorithm).toBeTruthy();
+    expect(isOffensive7).toBeTruthy();
     expect(build7.algorithm instanceof AlgorithmOffensive).toBeTruthy();
     expect(build9.algorithm).toBeTruthy();
+    expect(isOffensive9).toBeTruthy();
     expect(build9.algorithm instanceof AlgorithmOffensive).toBeTruthy();
   });
 
